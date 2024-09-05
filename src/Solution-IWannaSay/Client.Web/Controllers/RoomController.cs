5a684b6401e499d0843abb84bddcbfb8248c3ef6
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-05 10:18:14 +0300

    Задача : Создать Room controller и view

---
+++ src/Solution-IWannaSay/Client.Web/Controllers/RoomController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Web.Controllers;
+public class RoomController : Controller {
+    public IActionResult Index() {
+        return View();
+    }
+}
