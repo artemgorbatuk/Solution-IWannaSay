cf1b8cb1d2e4977f6fc90e342ba3be0009ec4396
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-05 10:10:49 +0300

    Задача : Проект Client.Web создан

---
+++ src/Solution-IWannaSay/Client.Web/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+#endregion
+
+#region Configure the HTTP request pipeline
+
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+#endregion
