0fd976ba5654ede40dc2c8967841a16a8fe37173
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-17 14:18:44 +0300

    Задача : Добавить простейшую Аутентификацию

---
+++ src/Solution-IWannaSay/Client.Web/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Services.Models;
+using System.Security.Claims;
+using Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Client.Web.Controllers;
+
+[Authorize]
+public class AccountController : Controller {
+
+    private readonly IServiceAuthentification serviceAuthentification;
+    private readonly ILogger<AccountController> logger;
+
+    public AccountController(IServiceAuthentification serviceAuthentification, ILogger<AccountController> logger) {
+        this.serviceAuthentification = serviceAuthentification;
+        this.logger = logger;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Login() {
+        return View();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login(LoginPage model) {
+
+        if (!ModelState.IsValid) {
+            ModelState.AddModelError(string.Empty, "НЕ заполнен логин и(или) пароль");
+            return View(model);
+        }
+
+        var isUserExist = await serviceAuthentification.IsUserExist(model.Login, model.Password);
+
+        if (!isUserExist) {
+            ModelState.AddModelError(string.Empty, "Некорректные логин и(или) пароль");
+            return View(model);
+        }
+
+        var identity = serviceAuthentification.GetIdentity(model.Login);
+
+        var principal = new ClaimsPrincipal(identity);
+
+        var properties = new AuthenticationProperties { IsPersistent = true };
+
+        await HttpContext.SignInAsync("Cookies", principal, properties);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    public async Task<IActionResult> Logout() {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Login", "Account");
+    }
+}
