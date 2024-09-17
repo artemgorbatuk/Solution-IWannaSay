6b120e9f848691024eee7de187b27279953f9757
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-17 14:22:05 +0300

    Задача : Сообщения как частичное представление

--- src/Solution-IWannaSay/Client.Web/Controllers/RoomController.cs
+++ src/Solution-IWannaSay/Client.Web/Controllers/RoomController.cs
@@ -1,19 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 
 namespace Client.Web.Controllers;
+
+[Authorize]
 public class RoomController : Controller {
 
     private readonly IServiceRoom serviceRoom;
+    private readonly IServiceMessage serviceMessage;
 
-    public RoomController(IServiceRoom serviceRoom) {
+    public RoomController(IServiceRoom serviceRoom, IServiceMessage serviceMessage) {
         this.serviceRoom = serviceRoom;
+        this.serviceMessage = serviceMessage;
     }
 
     public IActionResult Index() {
-
+        serviceRoom.ConnectAsync();
         var result = serviceRoom.GetRoomIndexPage();
-
         return View(result);
     }
+
+    public async Task<PartialViewResult> SendMessage(string message) {
+
+        await serviceMessage.SendMessageAsync(message);
+
+        var messages = serviceMessage.GetMessages();
+
+        var result = serviceRoom.GetRoomIndexPage();
+        var user = result.User;
+        var new_messages = result.Messages.ToList();
+        new_messages.Add(new() { Text = "Тест", User = user });
+        result.Messages = new_messages;
+
+        return PartialView("_Messages", result);
+    }
+
+
+    //public PartialViewResult GetMessages() {
+    //    var messages = serviceMessage.GetMessages();
+
+    //    var result = serviceRoom.GetRoomIndexPage();
+    //    var user = result.User;
+    //    var new_messages = result.Messages.ToList();
+    //    new_messages.Add(new() { Text = "Тест", User = user });
+    //    result.Messages = new_messages;
+
+    //    return PartialView("_Messages", result);
+    //}
+
+
+    //[HttpPost]
+    //public async Task<IActionResult> SendMessage(string message) {
+
+    //    await serviceMessage.SendMessageAsync(message);
+
+    //    return RedirectToAction("Index");
+    //}
 }
--- src/Solution-IWannaSay/Client.Web/wwwroot/js/room.js
+++ src/Solution-IWannaSay/Client.Web/wwwroot/js/room.js
@@ -1,5 +1,23 @@
 ﻿const messages = $('.app-messages');
 
 window.addEventListener('load', function () {
+    ScrollBottom();
+});
+
+function ScrollBottom() {
     messages.scrollTop(messages[0].scrollHeight);
+}
+
+$(document).ready(function () {
+    $('#buttonSend').click(function () {
+        //debugger;
+        var action = 'Room/SendMessage';
+        var message = $('#message').val();
+        $.get(action,
+            { message: message },
+            function (data) {
+                $('.app-messages').html(data); //replaceWith
+                ScrollBottom();
+            });
+    });
 });
