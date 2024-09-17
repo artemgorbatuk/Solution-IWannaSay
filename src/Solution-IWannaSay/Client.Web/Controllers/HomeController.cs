1cb834ece871e9bc321afa99965e13f7205b35a1
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-17 14:20:45 +0300

    Задача : Настроить вывод ошибки

--- src/Solution-IWannaSay/Client.Web/Controllers/HomeController.cs
+++ src/Solution-IWannaSay/Client.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 using Client.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Client.Web.Controllers;
+
+[Authorize]
 public class HomeController : Controller {
     private readonly ILogger<HomeController> _logger;
 
@@ -14,8 +18,20 @@ public class HomeController : Controller {
         return View();
     }
 
+    [AllowAnonymous]
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() {
+
+        var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+        if (exceptionHandlerPathFeature?.Error != null) {
+            var exception = exceptionHandlerPathFeature.Error;
+
+            _logger.LogError(exception, "An error occurred");
+
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier, ErrorMessage = exception.Message, StackTrace = exception.StackTrace });
+        }
+
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- src/Solution-IWannaSay/Client.Web/Models/ErrorViewModel.cs
+++ src/Solution-IWannaSay/Client.Web/Models/ErrorViewModel.cs
@@ -3,5 +3,7 @@ namespace Client.Web.Models;
 public class ErrorViewModel {
     public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    public string? ErrorMessage { get; set; }
+
+    public string? StackTrace { get; set; }
 }
