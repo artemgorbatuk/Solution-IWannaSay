e5393aa0d56046503afffb182e8e4d00a60c291f
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-09-19 18:05:57 +0300

    Устранение недочета : Сообщения на клиенте накапливаются не корректно.

--- src/Solution-IWannaSay/Repositories.SignalR/Api/ConnectorMessage.cs
+++ src/Solution-IWannaSay/Repositories.SignalR/Api/ConnectorMessage.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Logging;
 using Repositories.SignalR.Interfaces;
+using System.Collections.Concurrent;
 
 namespace Repositories.SignalR.Api;
 public class ConnectorMessage : IConnectorMessage {
@@ -8,18 +9,21 @@ public class ConnectorMessage : IConnectorMessage {
     private readonly HubConnection _connection;
     private readonly ILogger<ConnectorMessage> _logger;
 
+    public ConcurrentBag<string> _messages;
     public ConnectorMessage(HubConnection connection, ILogger<ConnectorMessage> logger) {
         _connection = connection;
         _logger = logger;
+
+        _messages = new ConcurrentBag<string>();
+
+        _connection.On("ReceiveMessage", (string message) => {
+            _messages.Add(message);
+            //_logger.LogInformation($"Connection: {_connection.ConnectionId}, receive message: {message}");
+        });
     }
 
     public IEnumerable<string> GetMessages() {
-        var messages = new List<string>();
-        _connection.On("ReceiveMessage", (string message) => {
-            messages.Add(message);
-            _logger.LogInformation($"Connection: {_connection.ConnectionId}, receive message: {message}");
-        });
-        return messages;
+        return _messages;
     }
 
     public async Task SendMessageAsync(string message) {
